54ee1e6501540bdaf3e63ee79b8316c690416b38
pandey-shailesh <pandeyshailesh2081998@gmail.com>
2023-06-20 16:21:45 +0530

    hooks and reflux

---
+++ router-in-react/speach-recoga-text/src/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { useSpeechRecognition } from "react-speech-kit";
+function App() {
+  const [text, setText] = useState();
+  const { listen, stop } = useSpeechRecognition({
+    onResult: (result) => setText(result),
+  });
+  return (
+    <div>
+      <h2>Convert Speech to Text: </h2>
+      <textarea value={text}></textarea>
+      <p>
+        <button onClick={listen}>Listen</button>
+        <button onClick={stop}>Stop</button>
+      </p>
+    </div>
+  );
+}
+const element = <App></App>;
+ReactDOM.render(element, document.getElementById("root"));
